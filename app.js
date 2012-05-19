4596c6e8a231e8bc0590a3faa338ece3260e12db
Vladimir Tsvang <vtsvang@gmail.com>
2012-05-19 04:35:55 +0400

    port = 3000

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var express = require('express')
   , routes = require('./routes');
 
 var app = module.exports = express.createServer();
-var port;
 // Configuration
 
 app.configure(function(){
@@ -21,12 +20,10 @@ app.configure(function(){
 
 app.configure('development', function(){
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-  port = 3000;
 });
 
 app.configure('production', function(){
   app.use(express.errorHandler());
-  port = 80;
 });
 
 // Routes
@@ -35,5 +32,5 @@ app.get('/', routes.index);
 app.get('/city/', routes.decode);
 app.get(/\/city\/([\d.\-]+)\,([\d.\-]+)\/?(\d*)\/?/, routes.city);
 
-app.listen(port);
+app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
